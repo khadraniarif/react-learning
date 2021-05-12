3c8aa08fc7d60a77303585464c48f48c5da98c8e
Arif Khadrani <khadraniarif@gmail.com>
2021-05-12 21:22:12 +0530

    add gitignore file

---
+++ my-app/src/components/button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = () => {
+    const buttonText = 'My Button';
+    return (
+        <button className='button'>{buttonText}</button>
+    );
+}
+
+export default Button;
